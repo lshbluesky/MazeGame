798b690a70d5d619791700cd818d9ee817e4b053
lshbluesky <61459016+lshbluesky@users.noreply.github.com>
2023-09-15 16:17:41 +0900

    메인 화면과 main 함수 정의
    main 함수를 정의하고 메인 화면의 기본적인 틀을 작성합니다.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+// 메인 화면을 그리기 위한 클래스 생성
+class StartMenu {
+private:
+	void DrawStartMenu() {
+		cout << "이 부분에 메인 화면을 구성합니다.\n";
+		getchar();
+	}
+public:
+	void InitStartMenu() {
+		StartMenu::DrawStartMenu();
+	}
+};
+
+int main(void)
+{
+	StartMenu MenuControl;
+	MenuControl.InitStartMenu();
+
+	system("cls");	
+	return 0;
+}
